61145333103c72335b9d9bef8300411f01968cba
Javier Martinez <javier.martinez@helisa.com>
2016-02-19 16:11:59 -0500

    rename 2

--- PocheWebSocket/src/co/com/poncho/websocket/Websocket.java
+++ PocheWebSocket/src/co/com/poncho/websocket/Websocket.java
@@ -1,24 +1,38 @@
 package co.com.poncho.websocket;
 
-import java.io.StringReader;
+import java.util.logging.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-import co.com.poncho.model.User;
+import com.ibm.wsdl.util.StringUtils;
 
-@ApplicationScoped
+//@ApplicationScoped
 @ServerEndpoint("/ponchito")
 public class Websocket {
+	
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+	 
+    @OnOpen
+    public void onConnectionOpen(Session session) {
+        logger.info("Connection opened ... " + session.getId());
+    }
+ 
+    @OnMessage
+    public String onMessage(String message) {
+    	System.out.println("me lo pela " + message);
+        return message;
+    }
+ 
+    @OnClose
+    public void onConnectionClose(Session session) {
+        logger.info("Connection close .... " + session.getId());
+    }
+	
+	/*
 
 	@Inject
     private UserSessionHandler sessionHandler;
@@ -62,5 +76,5 @@ public class Websocket {
                 sessionHandler.toggleUser(id);
             }
         }
-	}
+	}*/
 }
